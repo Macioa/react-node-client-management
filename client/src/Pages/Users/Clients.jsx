e342928afb814b11c555298c1733f5aba9dee380
Macioa <ryan.maciomaire@gmail.com>
2025-09-25 13:18:57 -0500

    edit client

--- client/src/Pages/Users/Clients.jsx
+++ client/src/Pages/Users/Clients.jsx
@@ -13,6 +13,7 @@ import { Dropdown, Menu, MenuButton, MenuItem, menuItemClasses } from "@mui/base
 import Filter from "./Filter";
 import User from "./User";
 import DeleteClient from "./Delete";
+import EditClient from "./Edit";
 
 const blue = {
   100: "#DAECFF",
@@ -139,6 +140,13 @@ const Clients = () => {
       headerClassName: "super-app-theme--header",
       renderCell: (params) => (
         <div className="flex gap-[10px]">
+          <Tooltip placement="top" title="Edit" arrow>
+            {" "}
+            <CiEdit
+              onClick={() => handleOpenEditModal(params.row)}
+              className="cursor-pointer text-green-500 text-[23px] hover:text-green-600"
+            />
+          </Tooltip>
           {
             loggedUser?.role != 'employee' &&
             <Tooltip placement="top" title="Delete" arrow>
@@ -185,7 +193,7 @@ const Clients = () => {
 
   return (
     <div className="w-full">
-
+      <EditClient open={openEditModal} setOpen={setOpenEditModal} userType="client" />
       <DeleteClient open={openDeleteModal} setOpen={setOpenDeleteModal} userId={selectedUserId} />
       <Filter open={openFilters} setOpen={setOpenFilters} />
       <User open={openUser} setOpen={setOpenUser} />
--- client/src/Pages/Users/Edit.jsx
+++ client/src/Pages/Users/Edit.jsx
@@ -22,7 +22,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="down" ref={ref} {...props} />;
 });
 
-const EditModal = ({ open, setOpen }) => {
+const EditModal = ({ open, setOpen, userType = "employee" }) => {
   /////////////////////////////////////// VARIABLES ///////////////////////////////////////
   const dispatch = useDispatch();
   const { currentEmployee, isFetching, error } = useSelector((state) => state.user);
@@ -32,29 +32,68 @@ const EditModal = ({ open, setOpen }) => {
     username: "",
     email: "",
     phone: "",
+    password: "",
+    city: "",
+    CNIC: "",
   };
 
   /////////////////////////////////////// STATES ///////////////////////////////////////
   const [employeeData, setEmployeeData] = useState(currentEmployee);
+  const [errors, setErrors] = useState({});
   /////////////////////////////////////// USE EFFECT ///////////////////////////////////////
   useEffect(() => {
     setEmployeeData(currentEmployee);
   }, [currentEmployee]);
 
   /////////////////////////////////////// FUNCTIONS ///////////////////////////////////////
+  const validateForm = () => {
+    const newErrors = {};
+    
+    if (!employeeData.firstName.trim()) {
+      newErrors.firstName = "First Name is required";
+    }
+    
+    if (!employeeData.lastName.trim()) {
+      newErrors.lastName = "Last Name is required";
+    }
+    
+    if (!employeeData.username.trim()) {
+      newErrors.username = "Username is required";
+    }
+    
+    if (!employeeData.phone.trim()) {
+      newErrors.phone = "Phone number is required";
+    }
+    
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    if (!validateForm()) {
+      return;
+    }
+    
     dispatch(updateUser(currentEmployee._id, employeeData, employeeData?.role));
     setEmployeeData(initialEmployeeState);
+    setErrors({});
     setOpen(false);
   };
 
   const handleInputChange = (field, value) => {
     setEmployeeData((prevFilters) => ({ ...prevFilters, [field]: value }));
+    // Clear error for this field when user starts typing
+    if (errors[field]) {
+      setErrors(prevErrors => ({ ...prevErrors, [field]: '' }));
+    }
   };
 
   const handleClose = () => {
     setOpen(false);
+    setEmployeeData(initialEmployeeState);
+    setErrors({});
   };
 
   return (
@@ -68,7 +107,7 @@ const EditModal = ({ open, setOpen }) => {
       maxWidth="sm"
       aria-describedby="alert-dialog-slide-description">
       <DialogTitle className="flex items-center justify-between">
-        <div className="text-sky-400 font-primary">Edit Employee</div>
+        <div className="text-sky-400 font-primary">Edit {userType === "client" ? "Client" : "Employee"}</div>
         <div className="cursor-pointer" onClick={handleClose}>
           <PiXLight className="text-[25px]" />
         </div>
@@ -77,7 +116,7 @@ const EditModal = ({ open, setOpen }) => {
         <div className="flex flex-col gap-2 p-3 text-gray-500 font-primary">
           <div className="text-xl flex justify-start items-center gap-2 font-normal">
             <PiNotepad size={23} />
-            <span>Employee Detials</span>
+            <span>{userType === "client" ? "Client" : "Employee"} Details</span>
           </div>
           <Divider />
           <table className="mt-4">
@@ -89,6 +128,8 @@ const EditModal = ({ open, setOpen }) => {
                   fullWidth
                   value={employeeData?.firstName}
                   onChange={(e) => handleInputChange("firstName", e.target.value)}
+                  error={!!errors.firstName}
+                  helperText={errors.firstName}
                 />
               </td>
             </tr>
@@ -100,6 +141,21 @@ const EditModal = ({ open, setOpen }) => {
                   fullWidth
                   value={employeeData?.lastName}
                   onChange={(e) => handleInputChange("lastName", e.target.value)}
+                  error={!!errors.lastName}
+                  helperText={errors.lastName}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="pb-4 text-lg">User Name </td>
+              <td className="pb-4">
+                <TextField
+                  size="small"
+                  fullWidth
+                  value={employeeData?.username}
+                  onChange={(e) => handleInputChange("username", e.target.value)}
+                  error={!!errors.username}
+                  helperText={errors.username}
                 />
               </td>
             </tr>
@@ -111,18 +167,20 @@ const EditModal = ({ open, setOpen }) => {
                     fullWidth
                     placeholder="Optional"
                     value={employeeData?.email}
-                    onChange={(e) => handleChange('email', e.target.value)}
+                    onChange={(e) => handleInputChange('email', e.target.value)}
                   />
                 </td>
               </tr>
             <tr>
-              <td className="pb-4 text-lg">User Name </td>
+              <td className="flex items-start pt-2 text-lg">Password </td>
               <td className="pb-4">
                 <TextField
+                  type="password"
+                  value={employeeData?.password}
+                  onChange={(e) => handleInputChange("password", e.target.value)}
                   size="small"
                   fullWidth
-                  value={employeeData?.username}
-                  onChange={(e) => handleInputChange("username", e.target.value)}
+                  placeholder="Leave blank to keep current password"
                 />
               </td>
             </tr>
@@ -135,6 +193,38 @@ const EditModal = ({ open, setOpen }) => {
                   value={employeeData?.phone}
                   onChange={(e) => handleInputChange("phone", e.target.value)}
                   fullWidth
+                  error={!!errors.phone}
+                  helperText={errors.phone}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="flex items-start pt-2 text-lg">City </td>
+              <td className="pb-4">
+                <CFormSelect
+                  size="sm"
+                  value={employeeData?.city}
+                  onChange={(e) => handleInputChange("city", e.target.value)}
+                  className="w-full"
+                >
+                  <option value="">Select City</option>
+                  {pakistanCities.map((city, index) => (
+                    <option key={index} value={city}>
+                      {city}
+                    </option>
+                  ))}
+                </CFormSelect>
+              </td>
+            </tr>
+            <tr>
+              <td className="flex items-start pt-2 text-lg">CNIC </td>
+              <td className="pb-4">
+                <TextField
+                  size="small"
+                  fullWidth
+                  placeholder="Optional"
+                  value={employeeData?.CNIC}
+                  onChange={(e) => handleInputChange("CNIC", e.target.value)}
                 />
               </td>
             </tr>
